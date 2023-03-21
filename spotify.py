69be3804ed74d4400adec5ade90f5965d9e5464f
pesto13 <ricola1304@gmail.com>
2023-03-21 14:15:06 +0100

    zzzz

--- spotify.py
+++ spotify.py
@@ -1,3 +1,4 @@
+import pprint
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -11,21 +12,34 @@ clientid = os.getenv('SPOTIPY_CLIENT_ID') # accede alla variabile d'ambiente MY_
 secret = os.getenv('SPOTIPY_CLIENT_SECRET')
 # Autenticazione all'API di Spotify
 
-# REDIRECT_URI = 'http://localhost:8888/callback'
+REDIRECT_URI = 'http://localhost:8888/callback'
 
 # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientid, client_secret=secret, redirect_uri=REDIRECT_URI))
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
+
+# sp = spotipy.Spotify(auth_manager=SpotifyOAuth(redirect_uri=REDIRECT_URI))
+
+
 auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-playlists = sp.user_playlists('pestobellodi')
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
+
+
+zz = sp.search('emberassed', limit=1, type='track')
+damn = zz['tracks']['items'][0]['uri']
+
+print(damn)
+
+# da quello che ho capito devo mettere URI altrimenti non posso fare il login :D
+print(sp.current_user())
+
+""" playlist = sp.user_playlist_create(user=sp.current_user()['id'], name='AAA')
+
+# Aggiungi le canzoni alla playlist
+sp.user_playlist_add_tracks(user=sp.current_user()['id'], playlist_id=playlist['id'], tracks=damn) """
+
+print(f"Playlist creata con successo!")
